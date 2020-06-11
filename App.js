2825ee03a4b146b7b117cc0a3c10d23adb9eac8a
aqsa249 <aqsamarib11@gmail.com>
2020-06-11 23:05:09 +0500

    update disply massaege

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1> welcome to my react app base</h1>
+    </div>
+  );
+}
+
+export default App;
