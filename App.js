189684808bfc583a5296e6367d8d9d874b76b530
aqsa249 <aqsamarib11@gmail.com>
2020-06-11 23:25:15 +0500

    update display image

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1> welcome to my react app base</h1>
+     <h1> welcome to my react base app deploying by aqsa mari</h1>
     </div>
   );
 }
