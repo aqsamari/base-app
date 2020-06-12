c312f73023fa6508f8a284274aa5d39565109289
aqsa249 <aqsamarib11@gmail.com>
2020-06-12 17:14:17 +0500

    update image logo

--- App.js
+++ App.js
@@ -4,8 +4,25 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-    welcome to my react base app deploying by aqsa mari
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+        <p>
+          Edit <code> src/App.js</code> and save to reloade.
+          <br>
+          update by Aqsa Mari
+          </br>
+        </p>
+        <a
+        className="app=link"
+        href="http://sudden-bootcamp.surge.sh/"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+          learn React
+        </a>
+      </header>
+  
     </div>
   );
 }
